372bfec73a12f27da243937e98769deab470b3b9
Felipe Pessina <fs.pessina@gmail.com>
2024-04-19 18:47:36 +0400

    Use correct account for sign transaction on signAndSendTransactions

--- src/lib/near-fastauth-wallet.ts
+++ src/lib/near-fastauth-wallet.ts
@@ -300,6 +300,7 @@ const FastAuthWallet: WalletBehaviourFactory<
 
     async signAndSendTransactions({ transactions, callbackUrl }) {
       const account = _state.wallet.account();
+      const connectedNearAccountWallet = _state.wallet._connectedAccount;
       const { accessKey } = await account.findAccessKey('', []);
 
       const needsFAK = transactions.some(({ receiverId }) => {
@@ -320,6 +321,7 @@ const FastAuthWallet: WalletBehaviourFactory<
         };
         const { closeDialog, signedTransactions, signedDelegates } =
           await _state.wallet.requestSignTransactions(arg);
+
         closeDialog();
         if (isRelayed) {
           signedDelegates.forEach((signedDelegate) =>
@@ -357,10 +359,17 @@ const FastAuthWallet: WalletBehaviourFactory<
               headers: new Headers({ 'Content-Type': 'application/json' }),
             });
           } else {
-            await account.signAndSendTransaction({
-              receiverId,
-              actions: actions.map((action) => createAction(action)),
-            });
+            const transaction =
+              // Disabling typescript to access a protected method and avoid code duplication. Open PR to allow access to signTransaction on near-api-js (https://github.com/near/near-api-js/blob/f28796267327fc6905a8c6a7051ff37aaa7bbd06/packages/accounts/src/account.ts#L145)
+              // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+              // @ts-ignore
+              await connectedNearAccountWallet.signTransaction(
+                receiverId,
+                actions.map((action) => createAction(action))
+              );
+            await _state.near.connection.provider.sendTransaction(
+              transaction[1]
+            );
           }
         }
       }
